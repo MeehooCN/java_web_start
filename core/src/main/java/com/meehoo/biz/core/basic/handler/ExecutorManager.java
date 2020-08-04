d2c209e4b4de5bd002aa2d867eea370de8206d81
MeehooCN <Meehoo2012!>
2020-08-04 14:25:48 +0800

    线程池工具类新增抛弃超时任务方法

--- core/src/main/java/com/meehoo/biz/core/basic/handler/ExecutorManager.java
+++ core/src/main/java/com/meehoo/biz/core/basic/handler/ExecutorManager.java
@@ -18,6 +18,7 @@ import java.util.function.Function;
 public class ExecutorManager {
 //    private static final ExecutorManager executorManager = new ExecutorManager();
     private Executor executor;
+    private static final int SleepTime = 2000;
 
     public ExecutorManager() {
         executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
@@ -50,6 +51,60 @@ public class ExecutorManager {
         for (FutureTask<V> future : taskList) {
             try {
                 resultList.add(future.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultList;
+    }
+
+    public <T,V> List<V> batchFunctionWithAbandon(List<T> source, Function<T,V> consumer){
+//        long nowMilis = System.currentTimeMillis();
+
+        List<FutureTask<V>> taskList = new ArrayList<>(source.size());
+        for (T t : source) {
+            Callable<V> callable = new Callable<V>() {
+                @Override
+                public V call() throws Exception {
+                    return consumer.apply(t);
+                }
+            };
+            FutureTask<V> task = new FutureTask<>(callable);
+            taskList.add(task);
+            exeFuture(task);
+        }
+
+        List<V> resultList = new ArrayList<>(source.size());
+        List<FutureTask<V>> undoneList = new ArrayList<>();
+        for (FutureTask<V> future : taskList) {
+            try {
+                if(future.isDone()){
+                    V e = future.get();
+                    resultList.add(e);
+                }else{
+                    undoneList.add(future);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (resultList.size()<source.size()){
+            try {
+                Thread.sleep(SleepTime);
+
+                for (FutureTask<V> future : undoneList) {
+                    if(future.isDone()){
+                        V e = future.get();
+                        resultList.add(e);
+                    }else{
+                        future.cancel(true);
+                    }
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (ExecutionException e) {

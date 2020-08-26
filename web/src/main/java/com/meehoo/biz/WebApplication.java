75f6f6ab4e8a362fc3665bebefc40e7b47d68016
MeehooCN <Meehoo2012!>
2020-08-26 09:16:48 +0800

    设置时区

--- web/src/main/java/com/meehoo/biz/WebApplication.java
+++ web/src/main/java/com/meehoo/biz/WebApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.TimeZone;
+
 /**
  * @author zc
  * @date 2020-01-03
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class WebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
         SpringApplication.run(WebApplication.class, args);
         System.out.println("-----------------启动成功了----------------");
     }
